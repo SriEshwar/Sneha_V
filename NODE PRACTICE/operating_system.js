691bcad1a75f98bf2304d480eea298ba81da1ab6
SNEHA V <107302558+sneha-venugopal@users.noreply.github.com>
2024-04-29 05:26:05 +0530

    Delete NODE PRACTICE directory

--- NODE PRACTICE/operating_system.js
+++
@@ -1,6 +0,0 @@
-const os = require('os')
-
-
-console.log(os.type()) //gives type of the os eg:Windows_NT
-console.log(os.version()) //prints version type eg:Windows 10 Home Single Language
-console.log(os.homedir()) //prints home directory eg:C:\Users\vinod
